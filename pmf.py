bb2cae0b405f0442ad7ecc59df2c7c5134569ed8
rio26 <xqli2626@gmail.com>
2018-12-29 16:28:02 +0800

    Converting matlab to python
    done half way through.

--- pmf.py
+++ pmf.py
@@ -4,12 +4,12 @@ import random, math
 import networkx as nx
 
 class PMF():
-	def __init__(self, A, train_size):
-		epsilon = 50 # Learning rate
-		reg = 0.01 # Regularization parameter
-		momentum=0.8; 
+	def __init__(self, A, train_size, epsilon=50, reg = 0.01, momentum=0.8, maxepoch = 50):
+		self.epsilon = epsilon # Learning rate
+		self.reg = reg # Regularization parameter
+		self.momentum=momentum 
 		epoch=1; 
-		maxepoch=50
+		self.maxepoch = maxepoch
 
 		# train - 70%; test - 30% 
 		self.Asize = A.shape[0]
@@ -46,13 +46,50 @@ class PMF():
 		for epoch in range(self.maxepoch):
 			rr = np.random.permutation(self.train_sample_size)
 			train_vector = self.train_sample[rr,:]
-			for batch in range(self.numbatches):
-				N = math.ceil(self.Asize/self.numbatches) # number training triplets per batch 
-				# print(N)
+			for batch in range(1, self.numbatches-2):
+				N = math.ceil(self.Asize/self.numbatches) # number training triplets per batch
+				aa_w = train_vector[((batch-1)*N+1):batch*N, 1]
+				aa_h = train_vector[((batch-1)*N+1):batch*N, 2]
+				aa = train_vector[((batch-1)*N+1):batch*N, 3]
+				aa = aa - self.mean_aij   # Default prediction is the mean of adjacency matrix A. 
+
+				''' Compute Prediction'''
+				pred_out = np.multiply(self.w1_W1[aa_w,:],self.w1_H1[aa_h,:]).sum(axis=1)
+				f = sum( np.power((pred_out - aa),2) + 0.5 * self.reg *((np.power(self.w1_W1[aa_w,:],2) + np.power(self.w1_H1[aa_h,:],2)).sum(axis=1)))
+
+				''' Compute Gradient'''
+				IO = np.tile(2*np.array([pred_out - aa]).T, (1, self.num_feature))
+				Ix_w = np.multiply(IO, self.w1_H1[aa_h,:]) + self.reg*self.w1_W1[aa_w,:]
+				Ix_h = np.multiply(IO,self.w1_W1[aa_w,:]) + self.reg*self.w1_H1[aa_h,:]
+				dw1_W1 = np.zeros((self.Asize,self.num_feature))
+				dw1_H1 = np.zeros((self.Asize,self.num_feature))
+
+				for ii in range(N-1):
+					dw1_W1[aa_w[ii],:] =  dw1_W1[aa_w[ii],:] +  Ix_w[ii,:]
+					dw1_H1[aa_h[ii],:] =  dw1_H1[aa_h[ii],:] +  Ix_h[ii,:]
+
+				''' Update movie and user features'''
+				self.w1_W1_inc = self.momentum* self.w1_W1_inc + self.epsilon*dw1_W1/N
+				self.w1_W1 = self.w1_W1 - self.w1_W1_inc
+
+				self.w1_H1_inc = self.momentum* self.w1_H1_inc + self.epsilon*dw1_H1/N
+				self.w1_H1 = self.w1_H1 - self.w1_H1_inc
+				
+				print(self.w1_W1)
+
+				"""
+				%%%% Update movie and user features %%%%%%%%%%%
+
+  				w1_M1_inc = momentum*w1_M1_inc + epsilon*dw1_M1/N;
+ 				 w1_M1 =  w1_M1 - w1_M1_inc;
+
+  				w1_P1_inc = momentum*w1_P1_inc + epsilon*dw1_P1/N;
+  				w1_P1 =  w1_P1 - w1_P1_inc;"""
+
 
 # read .gml
 G = nx.read_gml('data/dolphins-v62-e159/dolphins.gml') # 62 vertices
 A = nx.adjacency_matrix(G).todense()   #(62，62)   
 pmf = PMF(A, train_size=0.7)
 mat = pmf.read_triplets(file='edge.txt')
-print(type(mat))
+pmf.train()
