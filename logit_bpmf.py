c3dd7762f35b59325d8a52616c8d0662ab559666
Rio Li <xqli2626@gmail.com>
2019-01-13 00:34:52 +0800

    minor updates

--- logit_bpmf.py
+++ logit_bpmf.py
@@ -26,8 +26,8 @@ class LNMF():
 		self.mean_a = np.sum(A[:,2]) / self.Asize
 		self.gaussian_errors = gaussian_error(10000, 1) ## check sigma later
 		self.error_trend = np.zeros((max_iter))
-		
-		""" 
+
+		"""
 	    Parameters for Inverse-Wishart distribution
     	Assuming that parameters for both U and V are the same.
     	"""
@@ -42,7 +42,7 @@ class LNMF():
 		self.b0 = 2							# observation noise (precision)
 		df = r 								# degree of freedom
 		self.beta = self.b0 + self.r 		# fixed
-		self.nu = df + self.r 				# fixed 
+		self.nu = df + self.r 				# fixed
 
 		"""
 		Initialization Bayesian PMF using MAP solution found by PMF
@@ -55,11 +55,11 @@ class LNMF():
 		self.mu_w = np.array([w1_W1.mean(0)]).T
 		self.alpha_w = np.eye(r)
 		# self.alpha_w = inv(np.cov(w1_W1))
-		
+
 		self.mu_h = np.array([w1_H1.mean(0)]).T
 		self.alpha_h = np.eye(r)
 		# self.alpha_h = inv(np.cov(w1_H1))
- 
+
 		print("LNMF Initialization done.")
 
 	def train(self):
@@ -74,15 +74,15 @@ class LNMF():
 			w_bar = self.w1_W1_sample.mean(axis=1).reshape((-1,1))  # (n, 1)
 			w_cov = np.cov(self.w1_W1_sample) # (r,r)
 			WI_post = self.compute_wishart0(mat=self.WI_w_inv, n=N_w, cov=w_cov,mu0=self.mu0_w,s_bar=w_bar)
-			mu_tmp = ((self.b0*self.mu0_w + N_w*w_bar)/(self.b0+N_w)).reshape((-1,))	# [self.b0+N_w] can be substituded to [self.beta] 
+			mu_tmp = ((self.b0*self.mu0_w + N_w*w_bar)/(self.b0+N_w)).reshape((-1,))	# [self.b0+N_w] can be substituded to [self.beta]
 			self.mu_w, self.alpha_w, lamd_w = Gaussian_Wishart(mu_tmp, self.beta, WI_post, self.nu, seed=None)  # mean, matrix, covariance
 
 
 			""" Sample hyperparameter conditioned on the current ROW features."""
 			h_bar = self.w1_H1_sample.mean(axis=1).reshape((-1,1))  # (n, 1)
 			h_cov = np.cov(self.w1_H1_sample) # (r,r)
-			WI_post = self.compute_wishart0(mat=self.WI_h_inv ,n =N_h, cov=h_cov, mu0=self.mu0_h, s_bar=h_bar)	
-			mu_tmp = ((self.b0*self.mu0_w + N_h*h_bar)/(self.b0+N_w)).reshape((-1,))	# [self.b0+N_w] can be substituded to [self.beta] 
+			WI_post = self.compute_wishart0(mat=self.WI_h_inv ,n =N_h, cov=h_cov, mu0=self.mu0_h, s_bar=h_bar)
+			mu_tmp = ((self.b0*self.mu0_w + N_h*h_bar)/(self.b0+N_w)).reshape((-1,))	# [self.b0+N_w] can be substituded to [self.beta]
 			self.mu_h, self.alpha_h, lamd_h = Gaussian_Wishart(mu_tmp, self.beta, WI_post, self.nu, seed=None)  # mean (5,), matrix, covariance
 			print("size:", self.alpha_h.shape)
 
@@ -94,7 +94,7 @@ class LNMF():
 					tmp_mean = 1
 					wi_pdf = mul_normal.pdf(tmp_w.T, self.mu_w, lamd_w) #(r,)
 					# print(wi_pdf)
-					
+
 					for j in range(i, self.hsize):
 						mean_j = self.logit_nomral_mean(a=tmp_w, b=self.w1_H1_sample[:,j].reshape((-1,1)), error=self.gaussian_errors)
 						if(self.mat[i,j] == 1):
@@ -110,10 +110,10 @@ class LNMF():
 					tmp_w = self.w1_W1_sample[:,i].reshape((-1,1)).T  # (1,r)
 					tmp_mean = 0
 					wi_pdf = mul_normal.pdf(tmp_w.T, self.mu_w, lamd_w) #(r,)
-					
+
 					for j in range(self.hsize):
 						mean_j = self.logit_nomral_mean(a=tmp_w, b=self.w1_H1_sample[:,j].reshape((-1,1)), error=self.gaussian_errors)
-	
+
 
 # np.random.binomial(size=3, n=1, p= 0.5)
 
@@ -134,7 +134,7 @@ class LNMF():
 	def logistic(self,x):
 		return 1.0 / (1.0 + np.exp(-x))
 
-	def convert_triplets(file):	
+	def convert_triplets(file):
 		"""======= create a Triplets: {w_id, h_id, binary_link} ======="""
 		# with open('edge.txt', 'a') as f:
 		with open(file, 'a') as f:
